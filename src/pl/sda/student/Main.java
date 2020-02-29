9bae9d1fb86e6692b61bf8c45470e93e7799c662
MateuszMichalik93 <59762524+MateuszMichalik93@users.noreply.github.com>
2020-02-29 12:05:10 +0100

    dodani studenci + lista

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -22,13 +23,22 @@ public class Main {
 
 
         ObjectMapper objectMapper1 = new ObjectMapper();
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+        studentList.add(student5);
+        studentList.add(student6);
         try {
-            objectMapper1.writeValue(new File("student.json"), student1);
-            objectMapper1.writeValue(new File("student.json"), student2);
-            objectMapper1.writeValue(new File("student.json"), student3);
-            objectMapper1.writeValue(new File("student.json"), student4);
-            objectMapper1.writeValue(new File("student.json"), student5);
-            objectMapper1.writeValue(new File("student.json"), student6);
+            objectMapper1.writeValue(new File("student1.json"), student1);
+            objectMapper1.writeValue(new File("student2.json"), student2);
+            objectMapper1.writeValue(new File("student3.json"), student3);
+            objectMapper1.writeValue(new File("student4.json"), student4);
+            objectMapper1.writeValue(new File("student5.json"), student5);
+            objectMapper1.writeValue(new File("student6.json"), student6);
+            objectMapper1.writeValue(new File("studentList.json"), studentList);
         } catch (JsonMappingException e) {
             e.printStackTrace();
         }
