8188237036ba67012a0f4589ea99477f5b82d558
MateuszMichalik93 <59762524+MateuszMichalik93@users.noreply.github.com>
2020-02-29 11:36:42 +0100

    zaimportowano biblioteke json

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,8 +1,23 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Student student1 = new Student("Jan", "Kowalski", 32); //serializacja - przeksztalcenie obiektu
+        // serializujemy do formatu JASON
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("student.json"), student1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
